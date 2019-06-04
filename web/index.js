abef42c1291c9fd2f30b16a10bcc10c59f929f04
MuYu <mr.muzea@gmail.com>
2019-06-04 22:08:29 +0800

    feat: web

---
+++ web/index.js
@@ -0,0 +1,86 @@
+new Vue({
+  el: '#app',
+  data: function() {
+    return {
+      dialogFormVisible: false,
+      dialogConfirmVisible: false,
+      tableData: [],
+      formLabelWidth: '120px',
+      deleteLocal: '',
+      api: 'http://127.0.0.1:3001',
+      isAdd: false,
+      form: {
+        target: '',
+        local: '',
+      }
+    }
+  },
+  methods: {
+    handleFormAction() {
+      if (this.isAdd) {
+        this.handleAddAction();
+      } else {
+
+      }
+    },
+    handleEdit(local, target) {
+      this.form.local = local;
+      this.form.target = target;
+      this.isAdd = false;
+      this.dialogFormVisible = true;
+    },
+    handleDelete(local) {
+      this.deleteItem(local);
+    },
+    handleAdd() {
+      this.form.local = '';
+      this.form.target = '';
+      this.isAdd = true;
+      this.dialogFormVisible = true;
+    },
+    handleAddAction() {
+      this.dialogFormVisible = false;
+      this.addItem(this.form.local, this.form.target);
+    },
+    handleUpdateAction() {
+      this.dialogFormVisible = false;
+      this.updateItem(this.form.local, this.form.target);
+    },
+    async loadList() {
+      const resp = await (await fetch(`${this.api}/proxy`, { mode: 'cors' })).json();
+      const keys = Object.keys(resp);
+      this.tableData  = keys.map(key => {
+        return {
+          local: key,
+          target: resp[key],
+        };
+      });
+    },
+    async addItem(local, target) {
+      await fetch(`${this.api}/proxy`, {
+        method: 'POST',
+        body: JSON.stringify({ local, target }),
+        mode: 'cors',
+      });
+      this.loadList();
+    },
+    async updateItem(local, target) {
+      await fetch(`${this.api}/proxy/${local}`, {
+        method: 'PATCH',
+        body: JSON.stringify({ target }),
+        mode: 'cors',
+      });
+      this.loadList();
+    },
+    async deleteItem(local) {
+      await fetch(`${this.api}/proxy/${local}`, {
+        method: 'DELETE',
+        mode: 'cors',
+      });
+      this.loadList();
+    },
+  },
+  mounted: function() {
+    this.loadList();
+  }
+})
